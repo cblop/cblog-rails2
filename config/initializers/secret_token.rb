40d6580b832b340cb12b7b853f0f9070c42c21aa
cblop <commanderblop@gmail.com>
2012-06-23 09:29:11 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CblogRails2::Application.config.secret_token = 'f604d9ab2ecff35aa5be93f0cdb9009d793beeb66736db2253bdae21ef640d5db56c3d9069002073059c5434b24bba4e5fde9442245d505aafd83a4a42222ff5'
